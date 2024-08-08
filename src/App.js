fd5b8b5ff3e137a37eb3edb2b0956f076a706198
Fran <franbermejo.dam@gmail.com>
2024-08-08 17:17:11 +0200

    Subo el proyecto completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-screen bg-gray-900 text-white">
+      <Navbar></Navbar>
+      <div className="z-0 justify-center grid items-center mx-auto gap-3">
+        <h1 className="text-center text-5xl">Hello world!👋 <p> </p></h1>
+        <div className="flex w-1/3 items-center my-0 mx-auto gap-2">
+          {/* <img alt="una imagen" src="https://static8.depositphotos.com/1028979/1058/i/950/depositphotos_10580841-stock-photo-handsome-smiling-man-isolated-over.jpg" className="w-14"/> */}
+        </div>
+      </div>
     </div>
   );
 }
